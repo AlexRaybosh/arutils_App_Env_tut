e7f480fd4ffa9a4241d6386b2a0768235f5cb47e
Alex Raybosh <gopakita@protonmail.com>
2021-09-21 18:42:43 -0500

    updates

--- src/tut/Example07_DictionaryForCommonWords.java
+++ src/tut/Example07_DictionaryForCommonWords.java
@@ -4,11 +4,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import com.google.gson.JsonObject;
-
 import arapp.AppEnv;
 import arapp.etc.DictionaryWord;
-import arutils.util.JsonUtils;
 
 public class Example07_DictionaryForCommonWords {
 
@@ -21,38 +18,45 @@ public class Example07_DictionaryForCommonWords {
 		
 		int MAX=2000000;
 		
+		
 		System.out.println("About to shove "+MAX+" commonly used strings");
 		long start = System.currentTimeMillis();
 		List<DictionaryWord> words=new LinkedList<>(); 
-		for (int i=1;i<=MAX; ++i) {
-			DictionaryWord word = AppEnv.word("word #"+i);
+		for (int i=1;i<=MAX; ++i) {			 
+			String str=String.format("word #%08d",i);
+			DictionaryWord word = AppEnv.word(str);
 			words.add(word);
-			if (i==MAX) {
-				System.out.println("And the last one is: "+word.getId()+" - \""+word.getWord() +"\"");
-			}
 		}
+		
+		long addedFirstTime = System.currentTimeMillis();
+		System.out.println("Done creating "+MAX+" commonly used strings in "+(addedFirstTime-start)+" ms.");
+		
 		// lets ensure commit
 		// once getId/getWord returns, its guaranteed to be committed to the db
 		for (DictionaryWord w : words)
 			w.getId(); 
 		
-		long middle = System.currentTimeMillis();
-		System.out.println("Done creating/committing "+MAX+" commonly used strings in "+(middle-start)+" ms.");
+		long committedTime = System.currentTimeMillis();
+		System.out.println("Done committing "+MAX+" commonly used strings in "+(committedTime-addedFirstTime)+" ms.");
 		
 		
 		words.clear();
-		//MAX is twice greater than cacheSize, re-reading first half, as i go backwards
-		for (int i=MAX;i>0; --i) {
-			// we will probably re-read them, not fitting in the cache
-			DictionaryWord word = AppEnv.word("word #"+i);
+		for (int i=1;i<=MAX; ++i) {
+			// we will probably need to re-read them, if they dont fit in the cache
+			String str=String.format("word #%08d",i);
+			DictionaryWord word = AppEnv.word(str);
 			words.add(word);
 		}
-		// Ensure actual read happened
+		long addedBack=System.currentTimeMillis();
+		System.out.println("Done re-reading in " +(addedBack-committedTime)+" ms.");
+		
+		
+		// Ensure actual DB read happened
 		for (DictionaryWord w : words)
 			w.getId(); 
 		
 		long end = System.currentTimeMillis();
-		System.out.println("done re-reading ~ half of them in " +(end-middle)+" ms.");
+		System.out.println("Done DB resolving re-reads in " +(end-addedBack)+" ms.");
 
 		
 		System.out.println("Last 10 inserted");
@@ -69,21 +73,23 @@ public class Example07_DictionaryForCommonWords {
 	}
 /*
 About to shove 2000000 commonly used strings
-And the last one is: 32256371 - "word #2000000"
-Done creating/committing 2000000 commonly used strings in 24519 ms.
-done re-reading ~ half of them in 7396 ms.
+Done creating 2000000 commonly used strings in 11996 ms.
+Done committing 2000000 commonly used strings in 7815 ms.
+Done re-reading in 1675 ms.
+Done DB resolving re-reads in 179 ms.
 Last 10 inserted
-Row: {word=word #1999993, last_ms=1632073807062, id=32256702}
-Row: {word=word #1999872, last_ms=1632073807062, id=32256701}
-Row: {word=word #1999751, last_ms=1632073807062, id=32256700}
-Row: {word=word #1999630, last_ms=1632073807062, id=32256699}
-Row: {word=word #1999994, last_ms=1632073807062, id=32256698}
-Row: {word=word #1999873, last_ms=1632073807062, id=32256697}
-Row: {word=word #1999752, last_ms=1632073807062, id=32256696}
-Row: {word=word #1999631, last_ms=1632073807062, id=32256695}
-Row: {word=word #1999510, last_ms=1632073807062, id=32256694}
-Row: {word=word #1999991, last_ms=1632073807062, id=32256693}
+Row: {word=word #01999924, last_ms=1632267567036, id=179432572}
+Row: {word=word #01999925, last_ms=1632267567036, id=179432571}
+Row: {word=word #01999926, last_ms=1632267567036, id=179432570}
+Row: {word=word #01999927, last_ms=1632267567036, id=179432569}
+Row: {word=word #01999928, last_ms=1632267567036, id=179432568}
+Row: {word=word #01999929, last_ms=1632267567036, id=179432567}
+Row: {word=word #01999880, last_ms=1632267567036, id=179432566}
+Row: {word=word #01999881, last_ms=1632267567036, id=179432565}
+Row: {word=word #01999882, last_ms=1632267567036, id=179432564}
+Row: {word=word #01999883, last_ms=1632267567036, id=179432563}
 DB Profiler has been interrupted
+
  */
 
 }
